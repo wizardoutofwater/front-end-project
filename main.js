423090716d109c0f7cbd071013d9868879364856
Mike McDonald <whoiswizard@icloud.com>
2021-01-06 20:17:03 -0500

    Added basic search by ingredient functionality

--- main.js
+++ main.js
@@ -1,45 +1,43 @@
-$(document).ready(function(){
-    console.log('Ready!');
-    
-//  $('#movie-results').on('click', '.addBtn', function(){
-//         console.log('add to watchlist');
-//     });
+$(document).ready(function () {
+            console.log('Ready!');
+
+            //  $('#movie-results').on('click', '.addBtn', function(){
+            //         console.log('add to watchlist');
+            //     });
 
 
-$('#searchButton').click(function (event) {
-  event.preventDefault();
-  let $searchString = $("#search-bar").val();
-  // console.log('button clicked.');
-  // console.log($searchString)
+            $('#searchButton').click(function (event) {
+                    event.preventDefault();
+                    let $searchString = $("#search-bar").val();
+                    // console.log('button clicked.');
+                    // console.log($searchString)
 
-  const $movieResults = $('#movie-results');
-  const $jumboTron = $('.jumbotron');
+                    const $drinkResults = $('#drink-results');
+                    const $jumboTron = $('.jumbotron');
 
-  $.get(`https://www.omdbapi.com/?apikey=9c815c55&s=${$searchString}`).done(function(response){
-    console.log(response);
-    renderMovies(response.Search);
-    // let movieData = response.Search;
-  });
+                    $.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${$searchString}`).done(function (response) {
+                        console.log(response);
+                        renderMovies(response.drinks);
+                        // let movieData = response.Search;
+                    });
 
-  const renderMovies = (movieArray) => {
-    $jumboTron.css("display","none")
-    $movieResults.html('');
-   
-    movieArray.map((movie) => {
-      $movieResults.append(
-      `<div class="col-md-4">
+                    const renderMovies = (drinksArray) => {
+                        $jumboTron.css("display", "none")
+                        $drinkResults.html('');
+
+                        drinksArray.map((drink) => {
+                            $drinkResults.append(
+                                `<div class="col-md-4">
        <div class="card mb-4 box-shadow">
-          <img class="card-img-top" src="${movie.Poster}"
-              alt="movie poster">
+          <img class="card-img-top" src="${drink.strDrinkThumb}"
+              alt="${drink.strDrink} poster">
           <div class="card-body">
               <p>
-                  <div class="lead movie-title">${movie.Title}</div>
-                  <div class=" text-muted movie-release">${movie.Year}</div>
+                  <div class="lead movie-title">${drink.strDrink}</div>
+                  <div class=" text-muted movie-release"></div>
               </p>
               <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                      <a role="button" class="btn btn-sm btn-outline-secondary" href="https://www.imdb.com/title/${movie.imdbID}" target="_blank">View</a>
-                      <button id="${movie.imdbID}" type="button" class="btn btn-sm btn-outline-secondary addBtn">Add</button>
+                
                   </div>
                   
               </div>
@@ -47,23 +45,18 @@ $('#searchButton').click(function (event) {
       </div>
   </div>
       `)
-      $(`#${movie.imdbID}`).on("click", () => {
-        saveToWatchList(movieArray, `${movie.imdbID}`);
-      });
-    })
-  }
-});
+                            //   $(`#${movie.imdbID}`).on("click", () => {
+                            // saveToWatchList(movieArray, `${movie.imdbID}`);
+                        });
+                    }
+                });
+            });
+            
 
-// finish this watchlist function
-    const saveToWatchList = (movieArray, movieId) => {
-        console.log(movieId);
-        const movieToAdd = movieArray.find((movie) => movie.imdbID === movieId);
-        console.log(movieToAdd);
-        let watchListJSON = localstorage.getItem('watchlist');
-        let watchList = 
-
- }
-
-});
-
-
+        // finish this watchlist function
+        // const saveToWatchList = (movieArray, movieId) => {
+        //     console.log(movieId);
+        //     const movieToAdd = movieArray.find((movie) => movie.imdbID === movieId);
+        //     console.log(movieToAdd);
+        //     let watchListJSON = localstorage.getItem('watchlist');
+        //     let watchList = 
